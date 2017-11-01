a22078bc505e343cdfe6f511d8f448fa1337f0c2
zhaowebb <zhaowebb@gmail.com>
2017-11-01 15:46:38 -0400

    add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {ObjectId} = require('mongodb')
 var app = express();
 
 app.use(bodyParser.json());
@@ -31,8 +31,26 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectId.isValid(id)){
+		res.status(404).send();
+	} else{
+		Todo.findById(id).then( (todo) => {
+			if(!todo){
+				res.staus(404);
+			}
+			else{
+				res.send(todo);
+			}
+		}).catch((e) => {
+			res.status(400).send(e);
+		});
+	}
+
+});
 app.listen(3333, () => {
 	console.log('started on port 3333');
-})
+});
 
 
